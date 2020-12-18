320df1b9995c0ab134d4e4a352cbc614fb6f037c
Kyle Rader <kylewrader@gmail.com>
2020-12-17 17:59:47 -0800

    Start InputParser

---
+++ Breacher/InputParser.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Breacher
+{
+    public class InputParser
+    {
+        public int[,] ParseMatrix(string input)
+        {
+            var parts = input.Split(new[] { "\r\n", "\n", " " }, StringSplitOptions.RemoveEmptyEntries);
+            int n = (int)Math.Sqrt(parts.Length);
+            int[,] m = new int[n, n];
+            for (int i = 0; i < parts.Length; i++)
+            {
+                m[i / n, i % n] = int.Parse(parts[i], System.Globalization.NumberStyles.HexNumber);
+            }
+            return m;
+        }
+    }
+}
---
+++ BreacherTest/InputParserTest.cs
@@ -0,0 +1,27 @@
+﻿using Breacher;
+
+using FluentAssertions;
+
+using NUnit.Framework;
+
+namespace BreacherTest
+{
+    public class InputParserTest
+    {
+        [Test]
+        public void Parse_The_Matrix()
+        {
+            var input = @"1c 55 bd
+7a 1c e9
+e9 55 1c";
+
+            InputParser subject = new InputParser();
+            subject.ParseMatrix(input).Should().BeEquivalentTo(new[,]
+            {
+                {  28, 85, 189 },
+                { 122, 28, 233 },
+                { 233, 85,  28 },
+            });
+        }
+    }
+}
