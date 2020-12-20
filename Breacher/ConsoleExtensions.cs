be6e06dd635e20919e6bd7e0d3ecc8a832c7b978
Kyle Rader <kylewrader@gmail.com>
2020-12-19 22:52:28 -0800

    Add env var for setting your buffer size

---
+++ Breacher/ConsoleExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Breacher
+{
+    public static class ConsoleHelper
+    {
+        public static void WriteColor(string input, ConsoleColor color)
+        {
+            ConsoleColor original = Console.ForegroundColor;
+            Console.ForegroundColor = color;
+            Console.Write(input);
+            Console.ForegroundColor = original;
+        }
+
+        public static void Green(string input)
+        {
+            WriteColor(input, ConsoleColor.Green);
+        }
+
+        public static void DarkGray(string input)
+        {
+            WriteColor(input, ConsoleColor.DarkGray);
+        }
+
+        internal static void Red(string input)
+        {
+            WriteColor(input, ConsoleColor.Red);
+        }
+    }
+}
--- Breacher/Program.cs
+++ Breacher/Program.cs
@@ -10,27 +10,33 @@ namespace Breacher
 {
     class Program
     {
-        public const string Help = @"Usage: breacher
+        public const string BREACH_BUFFER_ENV_VAR = "BREACH_BUFFER";
+        public const int DEFAULT_BUFFER_SIZE = 4;
+        public const string Help = @"Usage:
 See https://github.com/kyle-rader/breacher/blob/main/README.md for full usage.
 
 You can enter input manually:
-  breacher
+  breach
 
 You can pass an input file:
-  breacher INPUT_FILENAME
+  breach INPUT_FILENAME
 
 Or you can pipe through stdin:
 
   windows:
-    type puzzle.txt | breacher
+    type puzzle.txt | breach
 
   osx or linux:
-    cat puzzle.txt | breacher
+    cat puzzle.txt | breach
 ";
 
         static void Main(string[] args)
         {
+            string version = Assembly.GetEntryAssembly().GetCustomAttribute<AssemblyInformationalVersionAttribute>().InformationalVersion;
+            string title = $"breach version: {version}";
+
             string filename = null;
+
             if (args.Length >= 1)
             {
                 switch (args[0].ToLower())
@@ -40,12 +46,13 @@ Or you can pipe through stdin:
                     case "-h":
                     case "-?":
                     case "/?":
+                        Console.WriteLine(title);
                         Console.WriteLine(Help);
                         Environment.Exit(0);
                         break;
                     case "-v":
                     case "--version":
-                        Console.WriteLine(Assembly.GetEntryAssembly().GetCustomAttribute<AssemblyInformationalVersionAttribute>().InformationalVersion);
+                        Console.WriteLine(version);
                         Environment.Exit(0);
                         break;
                     default:
@@ -53,6 +60,8 @@ Or you can pipe through stdin:
                         break;
                 }
             }
+
+            Console.WriteLine(title);
 
             Stopwatch stopWatch = new Stopwatch();
 
@@ -62,24 +71,24 @@ Or you can pipe through stdin:
                 if (File.Exists(filename))
                 {
                     stopWatch.Start();
-                    Console.WriteLine($"Reading input from {filename}");
+                    Console.WriteLine($"Reading input from: {filename}");
                     input = File.ReadAllText(filename);
                 }
                 else
                 {
-                    Console.Error.WriteLine($"Input file '{filename}' does not exist.");
+                    ConsoleHelper.Red($"Input file '{filename}' does not exist.");
                     Environment.Exit(1);
                 }
             }
             else if (Console.IsInputRedirected)
             {
                 stopWatch.Start();
-                Console.WriteLine("Reading from standard input");
+                Console.WriteLine("Reading input from: standard input");
                 input = Console.In.ReadToEnd();
             }
             else
             {
-                Console.WriteLine("You have opted to enter your puzzle manually. Enter your full puzzle input now.");
+                Console.WriteLine("Waiting for manual puzzle entry.");
                 Console.WriteLine("End your input with a single q at the end to finish entry and begin solving.");
                 StringBuilder sb = new StringBuilder();
                 char c;
@@ -93,31 +102,54 @@ Or you can pipe through stdin:
 
             // Split input on double newlines (separated by a single blank line).
             string[] inputSplit = input.Split(new[] { "\r\n\r\n", "\n\n" }, StringSplitOptions.RemoveEmptyEntries);
-            if (inputSplit.Length != 3)
+
+            /*
+             * Buffer size defaults to 4.
+             * Then override with the env var
+             * Final override with actual input
+             */
+            int bufferSize = DEFAULT_BUFFER_SIZE;
+            string bufferEnvVar = Environment.GetEnvironmentVariable(BREACH_BUFFER_ENV_VAR);
+            if (!string.IsNullOrWhiteSpace(bufferEnvVar) && int.TryParse(bufferEnvVar, out bufferSize))
             {
-                Console.Error.WriteLine("We failed to get all 3 input parts (buffer size, puzzle, and targets)");
-                Environment.Exit(1);
+                ConsoleHelper.DarkGray($"environment variable {BREACH_BUFFER_ENV_VAR} is set to {bufferSize}\n");
             }
 
-            int bufferSize = int.Parse(inputSplit[0].Trim());
-            int[,] puzzle = InputParser.ParseMatrix(inputSplit[1]);
-            PuzzleMatrix matrix = new PuzzleMatrix(puzzle);
-            Target[] targets = InputParser.ParseTargets(inputSplit[2]).ToArray();
+            int inputOffset = 0;
+            if (inputSplit.Length == 3)
+            {
+                inputOffset = 1;
+                int.TryParse(inputSplit[0].Trim(), out bufferSize);
+            }
 
+            int[,] puzzle = InputParser.ParseMatrix(inputSplit[0 + inputOffset]);
+            PuzzleMatrix matrix = new PuzzleMatrix(puzzle);
+            Target[] targets = InputParser.ParseTargets(inputSplit[1 + inputOffset]).ToArray();
+
+            Console.WriteLine($"Solving for buffer size: {bufferSize} ...");
+            bool notFound = true;
             foreach (var attempt in GenerateAttempts(targets).OrderBy(a => -a.Weight))
             {
                 if (matrix.Check(attempt.Chain, bufferSize, out var path))
                 {
-                    Console.WriteLine($"Found solution with weight {attempt.Weight} length: {path.Count}");
+                    notFound = false;
+                    Console.WriteLine($"Found solution with weight: {attempt.Weight} & length: {path.Count}");
                     matrix.Print(path.Reverse());
                     break;
                 }
             }
 
             stopWatch.Stop();
+
+            if (notFound)
+            {
+                ConsoleHelper.Red("Oh dear... There is no solution to this puzzle :(\nExit the interface and re-connect to get a different puzzle input.\n");
+            }
+
             // Get the elapsed time as a TimeSpan value.
             TimeSpan ts = stopWatch.Elapsed;
-            Console.WriteLine($"Solved in {ts.TotalMilliseconds:F2} ms");
+            Console.WriteLine($"Took {ts.TotalMilliseconds:F2} ms");
+
         }
 
         static IEnumerable<Attempt> GenerateAttempts(Target[] targets)
